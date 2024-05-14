62961364558d255e7749033186b4dca4ab652896
fhwarsam <fhwarsam@gmail.com>
2024-05-13 22:15:07 -0500

    Here is CMake test project

---
+++ functions.h
@@ -0,0 +1,5 @@
+#ifdef FUNCTIONS_H
+#define FUNCTIONS_H
+int add(int a, int b);
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "functions.cpp"
+
+int main(){
+
+std::cout << " Hello Cmakefile"<< "\n";
+std::cout << " The sum of 3 and 5 is"  << add(3,5)<< "\n";
+
+    return 0;
+}
